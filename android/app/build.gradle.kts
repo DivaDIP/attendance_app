4c265575b32da8ffd626c3e09c1cd2c665659eda
DivaDIP <hanladivsuhan@gmail.com>
2025-11-19 20:21:30 +0700

    creating camera_button widget

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.applicatin")
+    id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -25,7 +25,7 @@ android {
         applicationId = "com.example.attendance_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -44,6 +44,6 @@ flutter {
     source = "../.."
 }
 
-depencies {
+dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
 }
